5b83b4399629ce5964fed30a04dac8a7742a8fa3
monish <monish.barse@upcred.in>
2022-06-21 11:12:20 +0530

    login flow for user improved

--- app.js
+++ app.js
@@ -26,18 +26,16 @@ var userSchema = mongoose.Schema({
     city: String,
 });
 var user = mongoose.model("Ecommerce", userSchema);
-
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Viewall Users
-app.get("/viewall", (req, res) => {
-    console.log("Get View all called");
-    user.find(function (err, result) {
-        res.json(result)
-    });
-})
+// app.get("/getusers", (req, res) => {
+//     console.log("Get View all called");
+//     user.find(function (err, result) {
+//         res.json(result)
+//     });
+// })
 
 // Register New User
 app.post('/register', function (req, res) {
@@ -54,66 +52,87 @@ app.post('/register', function (req, res) {
         if (err)
             res.send("ERROR");
         else {
-            res.send("SUCCESS");
+            let dataOut = {
+                response_message: "Success",
+                response_data: {
+                    token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiYjc2ZjJiNTYtMmFiZS00MjE5LWI2NjktNDZmYzQ4MzM1OTI4Iiwicm9sZSI6IlNVUEVSX0FETUlOIiwiYWdlbnQiOiJQb3N0bWFuUnVudGltZS83LjI5LjAiLCJob3N0IjoiYXBpLnVwY3JlZC5haSIsImF1ZCI6ImFwaS51cGNyZWQuYWkiLCJleHAiOjE2NTYzOTA5NDh9.Gp4gpYQiHm0uBf4E2zpYsxEJwnswlbEfzJxvPk7WT1w",
+                    user_id: res._id,
+                    email: req.body.email
+                },
+                meta: {}
+            }
+            res.send(dataOut);
         }
     });
 });
 
 // Login Existing User
-app.post('/login', function (req, res) {
+app.post('/admin/login_admin', function (req, res) {
     loginid = req.body.email;
     loginpass = req.body.password;
-    console.log("post login all called" + "login email=" + loginid + "login password=" + loginpass );
-
+    console.log("post login all called" + "login-email=" + loginid + "  login-passw=" + loginpass);
     user.findOne({ email: loginid }, function (err, response) {
         if (err) throw err;
-        if (loginid == "admin" && loginpass == "admin") {
-            user.find(function (err, response) {
-                res.status(201).send(response);
-                console.log("admin called");
-            });
-        } else {
-            if (response !== null) {
-                if (loginid == response.email && loginpass == response.password) {
-                    console.log("update user called");
-                    res.send(response)
-
+        if (response !== null) {
+            if (loginid == response.email && loginpass == response.password) {
+                let dataOut = {
+                    response_message: "Success",
+                    response_data: {
+                        token: response._id,
+                        user_id: response._id,
+                        email: req.body.email
+                    },
+                    meta: {}
                 }
+                res.send(dataOut);
             } else {
-                res.send("Email is incorrect" + `<br>` + `<a href="/login">try again</a>`);
-                console.log("email incorect called");
+                let dataOut = {
+                    response_message: "Password Incorrect",
+                    response_data: {},
+                    meta: {}
+                }
+                res.send(dataOut);
+                console.log("Password incorrect called");
             }
+        } else {
+            let dataOut = {
+                response_message: "Email Not Found",
+                response_data: {},
+                meta: {}
+            }
+            res.send(dataOut);
+            console.log("email incorrect called");
         }
     });
 });
 // Edit User Data 
-app.post('/editprofile', function (req, res) {
-    
-    let useremail = req.body.email;
-    console.log("edit profile called "+ useremail);
-    user.findOneAndUpdate({ email: useremail }, {
-        name: req.body.name,
-        password: req.body.password,
-        number: req.body.number,
-        state: req.body.state,
-        city: req.body.city
-    }, function (err, response) {
-        if (err) throw err;
-        else res.send(response);
-    });
+// app.post('/editprofile', function (req, res) {
 
-});
+//     let useremail = req.body.email;
+//     console.log("edit profile called "+ useremail);
+//     user.findOneAndUpdate({ email: useremail }, {
+//         name: req.body.name,
+//         password: req.body.password,
+//         number: req.body.number,
+//         state: req.body.state,
+//         city: req.body.city
+//     }, function (err, response) {
+//         if (err) throw err;
+//         else res.send(response);
+//     });
 
-app.post('/deleteprofile', function (req, res) {
-    let useremail = req.body.email;
-    console.log("delete called ",useremail)
-    user.findOneAndRemove({ email: useremail }, {
-    }, function (err, response) { 
-        
-        if (err) throw err;
-        else res.send(response);
-    });
-   
-});
+// });
+
+// app.post('/deleteprofile', function (req, res) {
+//     let useremail = req.body.email;
+//     console.log("delete called ",useremail)
+//     user.findOneAndRemove({ email: useremail }, {
+//     }, function (err, response) { 
+
+//         if (err) throw err;
+//         else res.send(response);
+//     });
+
+// });
 
 app.listen(8080);
